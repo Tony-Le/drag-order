dbd7cdd4d2ecf8c27948c4c176ab265378fa1a72
Tony-Le <le.tony.05@gmail.com>
2018-01-04 14:41:16 -0500

    Update to drag-order

--- drag_order.js
+++ drag_order.js
@@ -1,16 +1,6 @@
-﻿window.onload = init();
-
-function init() {
-    /*These event listeners will be lost when swapped if not re-attached.
-    fixSwappedElem will re-attach the event listeners the elements marked
-    with the class "swap".*/
-    var list = document.getElementsByClassName("drag-order");
-    attachDragOrderEvents(list);
-}
-
-function attachDragOrderEvents (elemList)
-{
-    for (i = 0; i < elemList.length; i++) {
+function attachDragOrderEvents() {
+    var elemList = document.querySelectorAll(".drag-order");
+    for (var i = 0; i < elemList.length; i++) {
         elemList.item(i).setAttribute("draggable", "true");
         elemList.item(i).addEventListener("drag", function (event) { });
         elemList.item(i).addEventListener("dragstart", dragStarted);
@@ -20,22 +10,22 @@ function attachDragOrderEvents (elemList)
     }
 }
 
-function fixSwappedElems() {
-    var list = document.querySelectorAll(".drag-order.swap")
-    attachDragOrderEvents(list)
-    //Remove swap class from the respective dragorder elements
-    for (i = 0; i < list.length; i++) {
-        list.item(i).classList.remove("swap");
+function removeDragOrderEvents() {
+    var elemList = document.querySelectorAll(".drag-order");
+    for (var i = 0; i < elemList.length; i++) {
+        elemList.item(i).setAttribute("draggable", "false");
+        elemList.item(i).removeEventListener("drag", function (event) { });
+        elemList.item(i).removeEventListener("dragstart", dragStarted);
+        elemList.item(i).removeEventListener("dragend", dragEnded);
+        elemList.item(i).removeEventListener("dragover", draggingOver);
+        elemList.item(i).removeEventListener("drop", dropped);
     }
 }
-
 var dragged = null;
 
 function dragStarted(event) {
     if (event.target.classList.contains("drag-order")) {
         event.target.classList.add("drag-order-started");
-        event.target.classList.add("swap"); //Mark the element being swapped.
-        event.dataTransfer.setData("text", event.target.outerHTML);
         dragged = event.target;
     }
 }
@@ -43,22 +33,19 @@ function dragStarted(event) {
 function dragEnded(event) {
     if (event.target.classList.contains("drag-order")) {
         event.target.classList.remove("drag-order-started");
-        event.target.classList.remove("swap");
     }
 }
 
-/*dragover and drop events target usually target child elements instead. The functions 
+/*Dragover and drop events target usually target child elements instead. The functions 
 dragover and drop will then check parent class instead. Keeping the non child event
 in case it can be targeted.*/
 function isChildOfDragOrder(elem) {
     return elem.parentElement.classList.contains("drag-order");
 }
 
-/*Show that elements can be dragged into drag-over class elements or child of, only when dragged is set.
-Shows other draggable elements cannot be dragged in.*/
+/*Show that elements can be dragged into drag-over class elements or child of, only when dragged is set.*/
 function draggingOver(event) {
-    if (dragged !== null) 
-    {
+    if (dragged !== null) {
         if (event.target.classList.contains("drag-order")) {
             event.preventDefault();
         }
@@ -70,27 +57,30 @@ function draggingOver(event) {
 
 //Allow the drop event on drag-order elements that have drag-order class or are children, and when dragged is set.
 function dropped(event) {
-    if (dragged !== null)
-    {
+    if (dragged !== null) {
         if (event.target.classList.contains("drag-order")) {
             event.preventDefault();
             event.stopPropagation();
-            event.target.parentElement.classList.add("swap");
-            dragged.outerHTML = event.target.outerHTML;
-            event.target.outerHTML = event.dataTransfer.getData("text");
-            fixSwappedElems();
+            //Swap the elements
+            var temp = document.createElement("div");
+            dragged.parentNode.insertBefore(temp, dragged);
+            event.target.parentNode.insertBefore(dragged, event.target);
+            temp.parentNode.insertBefore(event.target, temp);
+            temp.parentNode.removeChild(temp);
         }
+            //drop event tends to fire over child elements instead
         else if (isChildOfDragOrder(event.target)) {
             event.preventDefault();
             event.stopPropagation();
-            if (event.target.parentElement.outerHTML != dragged.outerHTML) {
-                event.target.parentElement.classList.add("swap");
-                dragged.outerHTML = event.target.parentElement.outerHTML;
-                event.target.parentElement.outerHTML = event.dataTransfer.getData("text");
-                fixSwappedElems();
-            }
+            //Swap the elements
+            var realTarget = event.target.parentElement;
+            var temp = document.createElement("div");
+            dragged.parentNode.insertBefore(temp, dragged);
+            realTarget.parentNode.insertBefore(dragged, realTarget);
+            temp.parentNode.insertBefore(realTarget, temp);
+            temp.parentNode.removeChild(temp);
         }
         dragged = null; //dragged is no longer set.
     }
-    
+
 }
